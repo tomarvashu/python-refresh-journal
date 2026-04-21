e02c056238d5d5d814c49af23688ce5701f61e6d
Vashu Tomar <vashu.ch.28@gmail.com>
2026-04-21 16:36:03 +0530

    Add files via upload

---
+++ day1/strings.py
@@ -0,0 +1,15 @@
+# Strings Practice
+
+text = "Python Learning"
+
+print(text[0])
+print(text[-1])
+
+print(text[0:6])
+print(text[7:])
+
+print(text.lower())
+print(text.upper())
+print(text.replace("Python", "Java"))
+
+print(len(text))
---
+++ day1/useful_links.md
@@ -0,0 +1,8 @@
+# Useful Links
+
+## Python Docs
+https://docs.python.org/3/tutorial/
+
+## Practice
+https://www.w3schools.com/python/
+https://www.programiz.com/python-programming
