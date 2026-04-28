aafd55d26c663b4b8f60c0c0c6560e4db3382862
Vashu Tomar <vashu.ch.28@gmail.com>
2026-04-28 15:09:16 +0530

    Delete day 6 directory

--- day 6/append_file.py
+++
@@ -1,8 +0,0 @@
-# Day 6 - Appending to a File
-
-new_update = "\nSecond update: Follow-up assigned to service team."
-
-with open("daily_report.txt", "a") as file:
-    file.write(new_update)
-
-print("Update appended successfully.")
